50128afc51f29ec4d8818cb08139198cffe84f0e
Ataru Kodaka <ataru.kodaka@gmail.com>
2017-02-16 20:43:08 +0900

    fix

--- lib/middleman-akcms/series.rb
+++ lib/middleman-akcms/series.rb
@@ -6,6 +6,15 @@ module Middleman::Akcms::Series
     include Middleman::Akcms::Util
     include Contracts
 
+    Contract Middleman::Sitemap::Resource => Integer
+    def get_series_number(article)
+      series_number = article.data.series if article.data.series.is_a? Integer
+      series_number ||= article.data.series.number if article.data.series.is_a? Hash
+      series_number ||= (File.basename(article.path) =~ /^([0-9]+)[_\-\s]/) ? $1.to_i : 1
+
+      return series_number
+    end
+    
     Contract ResourceList => ResourceList
     def manipulate_resource_list(resources)
       series_title_template = app.config.akcms[:series][:title_template]
@@ -14,14 +23,15 @@ module Middleman::Akcms::Series
         yml = YAML::load(config_yml_res.render(layout: false))
         next unless yml['series']
 
-        dir = File.dirname(config_yml_res.path)
+        dir_path = File.dirname(config_yml_res.path)
+        dir_name = dir_path.split('/').last
+        series_name = yml['series'] || dir_name
         
-        series_name = yml['series'] || dir.split('/').last
-        
-        series_articles = resources.select {|r| r.is_article? && (! r.directory_index?) && File.dirname(r.path) == dir}
+        series_articles = select_articles(resources).select {|article|
+          (! article.directory_index?) && File.dirname(article.path) == dir_path}
         series_articles.each do |article|
-          binding.pry
-          series_number = article.data.series.try(:number) || ((File.split(article.path).last =~ /^([0-9]+)[_\-\s]/).nil? ? 0 : $1).to_i
+          series_number = get_series_number(article)
+          
           app.logger.debug(" -- series: #{series_name} [#{series_number}] #{article.title}")
           hash = {
             name: series_name,
