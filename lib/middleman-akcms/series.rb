ede8177ad70ecf2618b98d145d0cefe24347e9dc
Ataru Kodaka <ataru.kodaka@gmail.com>
2017-02-16 23:23:13 +0900

    fix

--- lib/middleman-akcms/series.rb
+++ lib/middleman-akcms/series.rb
@@ -48,37 +48,5 @@ module Middleman::Akcms::Series
 
       resources
     end
-=begin    
-    Contract ResourceList => ResourceList
-    def __manipulate_resource_list(resources)
-      series_articles = []
-
-      select_articles(resources).each do |article|
-        if article.data.series
-          name = article.metadata.try(:[], :directory).try(:[], :name) || File.dirname(article.path).split('/').last
-          # name = ((article.metadata.has_key?(:directory)) ? article.metadata[:directory][:name] : nil) || File.dirname(article.path).split('/').last
-          number = (File.split(article.path).last =~ /^([0-9]+)[_\-\s]/).nil? ? 0 : $1.to_i
-          
-          hash = { name: name, number: number}
-          title = apply_title(hash.merge(article_title: article.title))
-          article.add_metadata({page: { title: title }, locals: { series: hash }})
-          series_articles << article
-        end
-      end
-
-      series_articles.each do |res|
-        res_name = res.locals[:series][:name]
-        res.locals[:series][:articles] = series_articles.select {|r|
-          res_name == r.locals[:series][:name]
-        }
-      end
-      resources
-    end
-
-    Contract Hash => String
-    def apply_title(hash)
-      @app.config.akcms[:series][:title_template] % hash
-    end
   end # class
-=end
 end
